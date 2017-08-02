b81d780e07a118fc150292dfe02d545df1482edc
Mohan Sisodiya <msisodiya@vestmark.com>
2017-08-02 12:16:31 -0400

    Code cleanup and some validations

--- dev/index.jsx
+++ dev/index.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 import _ from 'lodash';
 import Autosuggest from 'react-autosuggest';
-import HelloWorld from './movieDetails.jsx';
+import MovieDetails from './movieDetails.jsx';
 
 class Search extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { data:'',  showResults: false, total_results: 0, releaseDate: '', id: '', list: '', term: '', sendMovieQuery: _.debounce((value) => {this.getMovie(value)}, 100)};
-    this.getMovie = this.getMovie.bind(this);
+    this.state = { data:'', showResults: false, total_results: 0, id: '', term: '' };
+    this.getMovieList = this.getMovieList.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -27,8 +27,9 @@ class Search extends Component {
     }
   }
 
-  getMovie(value) {
+  getMovieList(value) {
     var url = 'https://api.themoviedb.org/3/search/movie?query=' + value + '&api_key=989def4a1b101991db8e783b73ee7bd0';
+
       fetch(url)
       .then((response) => response.json())
       .then((responseJson) => {
@@ -39,11 +40,9 @@ class Search extends Component {
     })
   }
 
-  movieDetails(value) {
-    var url = 'https://api.themoviedb.org/3/movie/' + parseInt(value) + '?api_key=989def4a1b101991db8e783b73ee7bd0&language=en-US';
-
-    //jQuery("#mm").html("");
-      var enteredMovie = value, results;
+  getmovieDetails(value) {
+    var url = 'https://api.themoviedb.org/3/movie/' + parseInt(value) + '?api_key=989def4a1b101991db8e783b73ee7bd0&language=en-US',
+      enteredMovie = value, results;
 
       fetch(url)
       .then((resp) => resp.json())
@@ -55,36 +54,35 @@ class Search extends Component {
           vote_average: responseJson.vote_average,
           poster_path: responseJson.poster_path,
           runtime: responseJson.runtime,
-          revenue: responseJson.revenue,
-          repeatTitle: responseJson.title
+          revenue: responseJson.revenue
         });
       });
   this.setState({ showResults: false });
   }
 
   componentDidMount() {
-    this.movieDetails(278927);
+    this.getmovieDetails(278927);
   }
 
   onInputChange(value) {
     if(value.length > 0 && this.state.total_results > 0) {
       this.setState({ showResults: true });
     } else {
-    this.setState({ showResults: false });
+      this.setState({ showResults: false });
     }
     this.setState({ term: value });
-    this.getMovie(value);
+    this.getMovieList(value);
   }
 
   onItemClick(value, id) {
     this.setState({ term: value, id: id});
-    this.movieDetails(id);
+    this.getmovieDetails(id);
   }
 
   render() {
-    var term = this.state.term, self = this;
+    var term = this.state.term, self = this, movieList;
 
-    var movieList =  _.map(this.state.data, function(obj) {
+    movieList =  _.map(this.state.data, function(obj) {
       if(_.includes(obj.title.toLowerCase(), term.toLowerCase().trim())) {
         return (<li className="suggestions" key={obj.id} id={obj.id} onClick={event => self.onItemClick(obj.title, obj.id)}>{obj.title}</li>);
       }
@@ -96,7 +94,7 @@ class Search extends Component {
           <input className="movieInput" placeholder="Search Movie..."  value={this.state.term} id={this.state.id} onInput={event => this.onInputChange(event.target.value)} />
           {self.state.showResults ? <ul className="suggestionDropDown">{movieList}</ul> : "" }
         </div>
-          <HelloWorld data={this.state}/>
+          <MovieDetails data={this.state}/>
       </div>
     );
   }
--- dev/movieDetails.jsx
+++ dev/movieDetails.jsx
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import numeral  from "numeral";
 
-class HelloWorld extends Component{
-componentWillMount() {
-  document.addEventListener('click', this.handleClick, false);
-}
+class MovieDetails extends Component{
+	componentWillMount() {
+	  document.addEventListener('click', this.handleClick, false);
+	}
 
-componentWillUnmount() {
-  document.removeEventListener('click', this.handleClick, false);
-}
+	componentWillUnmount() {
+	  document.removeEventListener('click', this.handleClick, false);
+	}
+
+	validations(value) {
+		if(value === undefined || value === '' || value === 0) {
+			return '--';
+		} else {
+			return value;
+		}
+	}
+	
     render() {
-     let posterIMG = 'https://image.tmdb.org/t/p/w342' + this.props.data.poster_path;
-        return (
-          <div className="detailsContainer">
-          <div className="detailsInfo">
-              <h1>{this.props.data.title}</h1>
-              <p>{this.props.data.overview}</p>
-              <div>Release date: <span>{this.props.data.release_date}</span></div>
-              <div>Vote Average: <span>{this.props.data.vote_average}</span></div>
-              <div>Running Time: <span>{this.props.data.runtime}</span></div>
-              <div>Box Office: <span>{this.props.data.revenue}</span></div>
-              <p>{this.props.poster_path}</p>
-              </div>
-              <div><img id="postertest" className='poster' src={posterIMG}/></div>
-          </div>
-        )
+		let posterIMG = 'https://image.tmdb.org/t/p/w342' + this.props.data.poster_path,
+			data = this.props.data;
+
+		data.revenue = this.validations(data.revenue);
+		data.release_date = this.validations(data.release_date);
+		data.runtime = this.validations(data.runtime);
+		data.vote_average = this.validations(data.vote_average);
+
+		if(data.revenue !== '--') {
+			data.revenue = numeral(data.revenue).format('($0,0)');
+		}
+
+		if(data.poster_path === undefined || data.poster_path === null) {
+			posterIMG = './No_Photo_Available.png';
+		}
+
+		return (
+			<div className="detailsContainer">
+				<img id="postertest" className='poster' src={posterIMG}/>
+				<div className="detailsInfo">
+					<h1>{this.props.data.title}</h1>
+					<div className="movieOverview">{this.props.data.overview}</div>
+					<div className="detailsDiv">Release date: <span>{data.release_date}</span></div>
+					<div className="detailsDiv">Running Time: <span>{data.runtime}</span></div>
+					<div className="detailsDiv">Vote Average: <span>{data.vote_average}</span></div>
+					<div className="detailsDiv">Box Office: <span>{data.revenue}</span></div>
+				</div>
+			</div>
+		)
     }
 }
 
-export default HelloWorld
+export default MovieDetails
